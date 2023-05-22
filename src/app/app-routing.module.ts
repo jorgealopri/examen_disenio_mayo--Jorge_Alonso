f21fd656ad4877afe5542413b1933eba330990aa
jorge <alonsoojorge120@gmail.com>
2023-05-22 21:10:55 +0200

    first

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InicioComponent } from './vistas/inicio/inicio.component';
+import { GaleriaComponent } from './vistas/galeria/galeria.component';
+import { DivisasComponent } from './vistas/divisas/divisas.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "inicio", component:InicioComponent
+  },{
+    path: "galeria", component:GaleriaComponent
+  },{
+    path: "divisas", component:DivisasComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,37 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './componentes/header/header.component';
+import { FooterComponent } from './componentes/footer/footer.component';
+import { InicioComponent } from './vistas/inicio/inicio.component';
+import { GaleriaComponent } from './vistas/galeria/galeria.component';
+import { DivisasComponent } from './vistas/divisas/divisas.component';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input';
+
+import {MatCardModule} from '@angular/material/card';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    InicioComponent,
+    GaleriaComponent,
+    DivisasComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatCardModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
